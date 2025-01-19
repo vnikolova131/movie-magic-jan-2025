60b227eaf011cc705ff5bcc04bf4476f381e8fee
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-01-19 19:48:41 +0200

    install express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello bicheez")
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("IT WORKS bichezzzz!")
+})
+
+
+app.listen(5000, () => console.log("Server is listening on http://localhost:5000..."))
