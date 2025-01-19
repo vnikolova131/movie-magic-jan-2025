c73462da2fee2f3c922b7f911fdf1e664be8c57d
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-01-19 22:44:57 +0200

    show home page

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.engine('hbs', handlebars.engine ({
 app.set('view engine', 'hbs')
 app.set('views','./src/views')
 
+app.use('/static', express.static('src/public'))
+
 
 app.get('/', (req,res) => {
     res.render('home', { layout: false })
