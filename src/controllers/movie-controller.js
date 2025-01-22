c56500775dc3c33d0f2ce22d113ad93d2e57472f
vnikolova131 <130762543+vnikolova131@users.noreply.github.com>
2025-01-22 20:54:29 +0200

    middleware for create

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -7,7 +7,16 @@ movieController.get('/create', (req,res) => {
     res.render('create');
 })
 
+movieController.post('/create', (req,res) => {
+    const newMovie = req.body;
+
+    //console.log(req.body)
+    res.end()
+})
+
 movieController.get('/:movieId/details', (req, res) => {
+
+    const movieId = req.params.movieId;
     res.render('details')
 })
 
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.set('view engine', 'hbs')
 app.set('views','./src/views')
 
 app.use('/static', express.static('src/public'))
+app.use(express.urlencoded({extended: false}))
 
 app.use(routes)
 
